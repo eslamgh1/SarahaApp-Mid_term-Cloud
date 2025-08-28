78dea55975e29560a09f4d16b527377ef1c0e4da
Eslam Gomaa <148918144+eslamgh1@users.noreply.github.com>
2025-08-28 13:28:34 +0300

    update-28-8

--- src/app.controller.js
+++ src/app.controller.js
@@ -2,44 +2,37 @@ import connectDB from "./DB/connection.js";
 import { globalErrorHandling } from "./Middlewares/globalErrorHandling.js";
 import messageRouter from "./Modules/Messages/message.controller.js";
 import userRouter from "./Modules/Users/user.controller.js";
-import cors from 'cors';
-import morgan from 'morgan'
-import { rateLimit } from 'express-rate-limit'
+import cors from "cors";
+import morgan from "morgan";
+import { rateLimit } from "express-rate-limit";
 import helmet from "helmet";
 
-
-
-
-var whitelist = ['http://localhost:4200', 'http://localhost:3000'];
+var whitelist = ["http://localhost:4200", "http://localhost:3000"];
 var corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
-  }
+  },
 };
 
-
 const limiter = rateLimit({
-	windowMs:  60 * 1000, // 
-	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-message:{
-  error:"Too many requests"
-}
-})
-
-
-
+  windowMs: 60 * 1000, //
+  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: {
+    error: "Too many requests",
+  },
+});
 
 const bootstrap = (app, express) => {
   app.use(cors(corsOptions));
   // app.use(morgan("combined"))
   // Apply the rate limiting middleware to all requests.
-  app.use(limiter)
+  app.use(limiter);
   app.use(helmet());
-//* it’s  parsing [ JSON string to JavaScript object]
+  //* it’s  parsing [ JSON string to JavaScript object]
   app.use(express.json());
   connectDB();
 
@@ -47,6 +40,9 @@ const bootstrap = (app, express) => {
   app.use("/users", userRouter);
   app.use("/messages", messageRouter);
 
+  app.get("/", (req, res) => {
+    res.json({ message: "Welcome on my app 💙" });
+  });
   app.all("/*demo", (req, res, next) => {
     throw new Error("Page is not found", { cause: 404 });
   });
